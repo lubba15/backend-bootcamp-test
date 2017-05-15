74baa7028094b8f8375ae874373c0fe55dc3292a
lubba15 <lubabalomkiva15@gmail.com>
2017-05-15 15:42:59 +0200

    changes

--- countAllPaarl.js
+++ countAllPaarl.js
@@ -1,4 +1,4 @@
-module.export = function(numberPlate) {
+module.exports = function(numberPlate) {
   var array = [];
   var number = numberPlate.split(', ');
   for (var i = 0; i < number.length; i++) {
